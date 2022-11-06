e0a653716fffed9a57bf5df84e083ac22c7206d1
Miguel Macedo Ferreira <111234286+Maccelerator@users.noreply.github.com>
2022-11-06 03:04:05 -0300

    Arquivo index.js da pagina de cadastro
    Erros na pagina serão corrigidos posteriormente

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -3,8 +3,59 @@ import Header from "../../components/Header";
 function Signup(){
     return(
         <div>
-            <Header/>
-            <h1></h1>
+            <h1>Cadastro</h1>
+            <div class="box">
+            <form action="form.php" method="POST" class="main-form">
+                <div class="form-group">
+                    <label>Primeiro Nome: </label>
+                    <input type="text" placeholder="Escreva seu primeiro nome."/>
+                </div>
+
+                <div class="form-group">
+                    <label>Sobrenome: </label>
+                    <input type="text" placeholder="Escreva o seu sobrenome."/>
+                </div>
+
+                <div class="form-group">
+                    <label>Gênero: </label>
+                    <select name="genero">
+                        <option value="masculino">Masculino</option>
+                        <option value="feminino">Feminino</option>
+                        <option value="outro">Outro</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label>Idade: </label>
+                    <input type="number" name="idade" placeholder="Escreva sua idade."/>
+                </div>
+
+                <div class="form-group">
+                    <label>Primeiro Nome:</label>
+                    <input type="text" placeholder="Escreva seu primeiro nome."/>
+                </div>
+
+                <div class="form-group">
+                    <label>email:</label>
+                    <input type="email" placeholder="Ex email@gmail.com."/>
+                </div>
+
+                <div class="form-group">
+                    <label>Senha:</label>
+                    <input type="password" placeholder="Informe uma senha."/>
+                </div>
+
+                <div class="form-group">
+                    <label>Repete Senha:</label>
+                    <input type="password" placeholder="\por favor, repita sua senha."/>
+                </div>
+
+                <div class="buttom">
+                    <input type = "submit" name="submit" value="Submit"/>
+                </div>
+
+            </form>
+            </div>
         </div>
     );
 };
