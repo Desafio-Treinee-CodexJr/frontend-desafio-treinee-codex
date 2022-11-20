8fc2280aedd60809d0599dcc0978247a9a62d0dd
Samuel Lucas <samuellucasvmcontato@gmail.com>
2022-11-20 12:30:49 -0300

    style: changing home css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,14 +10,14 @@ function Home(props) {
 
     async function addNewItem() {
         const data = { text: newItem }
-          
+
         const options = {
             headers: {
                 "Content-Type": "application/json",
                 "authorization": "Bearer " + localStorage.getItem("token")
             }
         };
-        const response = await api.post("/task", data, options );
+        const response = await api.post("/task", data, options);
         const tasks = response.data.tasks;
         setToDoItems(tasks);
         setNewItem("");
@@ -51,6 +51,16 @@ function Home(props) {
         setList();
     }, [])
 
+    async function setDone(id) {
+        const response = await api.put(
+            "/task", {
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": "Bearer " + localStorage.getItem("token")
+            },
+        });
+    }
+
     return (
         <>
             <Header />
@@ -62,6 +72,7 @@ function Home(props) {
                 <h3>Lista:</h3>
                 {toDoItems.map((task) => (
                     <li className="todo-task" key={task.id}>
+                        <input type='checkBox' onChange={() => setDone()} id="done-input"></input>
                         {task.text}
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" id="trash-icon" viewBox="0 0 16 16" onClick={() => deleteItem(task.id)}>
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
