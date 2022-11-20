d38fe801f9c89b98812ebd56cc9e3aac09bab284
Samuel Lucas <samuellucasvmcontato@gmail.com>
2022-11-20 20:44:01 -0300

    refactor: removing unused functions

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
 import SignButton from "../../components/SignButton";
 import SignInput from "../../components/SignInput";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import InvalidityMsg from "../../components/InvalidityMsg";
 import { validateEmail, validatePassword } from "../../util/validation";
 import "./styles.css";
@@ -9,16 +9,6 @@ import Header from "../../components/Header";
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
-/*     useEffect(() => {
-        axios.get("https://backend-desafio-treinee-codex.herokuapp.com/users")
-            .then((response) => {
-                console.log(response.data)
-            })
-            .catch(() => {
-                console.log("Erro ao conectar")
-            })
-    }, []) */
-
     const [email, setEmail] = useState({ value: "", invalidity: "" });
     const [password, setPassword] = useState({ value: "", invalidity: "" });
 
@@ -56,7 +46,6 @@ function Login() {
                     localStorage.setItem("token", token);
                     localStorage.setItem("user", JSON.stringify(response));
                     localStorage.setItem("logged", "true");
-                    const usertest = response.data; 
                     navigate('/home');
                 })
                 .catch((error) => {
