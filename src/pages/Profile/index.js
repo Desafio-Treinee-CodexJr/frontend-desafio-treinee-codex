6761500ef2d55228a1c6a96bfabac49cec435860
Samuel Lucas <samuellucasvmcontato@gmail.com>
2022-11-20 20:19:26 -0300

    refactor: removing variables unused

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,12 +3,9 @@ import React from "react";
 import api from "../../service/index";
 import { useState, useEffect } from "react";
 import "./styles.css";
-import { useNavigate } from 'react-router-dom';
 
 function Profile() {
     const [profileInfo, setProfileInfo] = useState([]);
-    const [name, setName] = useState();
-    const [age, setAge] = useState();
     const [photo, setPhoto] = useState();
 
     async function changeName(e) {
@@ -21,7 +18,6 @@ function Profile() {
             }
         });
         name = response.data.name;
-        setName(name);
     };
 
     async function changeAge(e) {
@@ -34,13 +30,11 @@ function Profile() {
             }
         });
         age = response.data.age;
-        setAge(age);
     };
 
     async function changePhoto(e) {
         let photo = e.target.value;
         setPhoto(photo);
-        console.log(photo)
     };
 
     async function getUserInfo() {
@@ -51,10 +45,8 @@ function Profile() {
         setProfileInfo(response)
     }
 
-    const navigate = useNavigate();
-
     async function sendPhoto() {
-        const response = await api.put("/users/info", { photo: photo }, {
+        await api.put("/users/info", { photo: photo }, {
             headers: {
                 "Content-Type": "application/json",
                 "authorization": "Bearer " + localStorage.getItem("token")
@@ -71,7 +63,7 @@ function Profile() {
         <>
             <Header />
             <div id='profile-infos'>
-                <img id='profile-photo' src={profileInfo.data && profileInfo.data.photo}></img>
+                <img id='profile-photo' src={profileInfo.data && profileInfo.data.photo} alt='Foto do usuário'></img>
                 <input type="text" id="photo-input" placeholder="Coloque a URL de sua foto" onChange={changePhoto}></input>
                 <button id="send-photo-button" onClick={sendPhoto}>Enviar foto</button>
                 <div id='profile-inputs'>
