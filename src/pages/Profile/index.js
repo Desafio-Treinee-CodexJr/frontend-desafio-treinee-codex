acaa3545706f2f0c75d4decea61836f2807e5276
Samuel Lucas <samuellucasvmcontato@gmail.com>
2022-11-20 09:03:07 -0300

    feat: taking userinfo from db on profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,30 @@
 import Header from "../../components/Header";
 import React from "react";
+import api from "../../service/index";
+import { useState, useEffect } from "react";
+
 
 function Profile(props) {
+    let [profileInfo, setProfileInfo] = useState([]);
+
+    async function getUserInfo() {
+        const response = await api.get(
+            "/users/info",
+            { headers: { "Content-Type": "application/json", "authorization": "Bearer " + localStorage.getItem("token") } },
+        );
+        setProfileInfo(response)
+    }
+
+    useEffect(() => {
+        getUserInfo();
+    }, [])
+
     return (
-        <Header />
+        <>
+            <Header />
+            <button onClick={() => console.log(profileInfo.data)}></button>
+            <img href=''></img>
+        </>
     );
 };
 
