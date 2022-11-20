beabb5acfe16790c8ba64d377450774fe9496d3c
Samuel Lucas <samuellucasvmcontato@gmail.com>
2022-11-20 20:44:52 -0300

    feat: creating edit task text function

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -27,6 +27,18 @@ function Home() {
         e.preventDefault();
     };
 
+    async function changeTask(e, id) {
+        let text = e.target.value;
+        const response = await api.put("/task/text", { id: id, text: text }, {
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": "Bearer " + localStorage.getItem("token")
+            }
+        });
+        const tasks = response.data.tasks;
+        setToDoItems(tasks);
+    };
+
     async function deleteItem(id) {
         const response = await api.delete("/task", {
             headers: {
@@ -75,7 +87,7 @@ function Home() {
                 <h3>Lista:</h3>
                 {toDoItems.map((task) => (
                     <li className={task.done ? "task-done todo-task" : "todo-task"} key={task.id}>
-                        {task.text}
+                        <input id="task-input" defaultValue={task.text} onChange={(e) => changeTask(e, task.id)} ></input>
                         <svg onClick={() => doTask(task.id)} className="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.25 8.891l-1.421-1.409-6.105 6.218-3.078-2.937-1.396 1.436 4.5 4.319 7.5-7.627z"/></svg>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="icon" viewBox="0 0 16 16" onClick={() => deleteItem(task.id)}>
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
