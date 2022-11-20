9e5cd4bab6fafcbb0bdd95b832976699848dc6d9
Gustavo <gustavo.soares@ccc.ufcg.edu.br>
2022-11-19 22:29:21 -0300

    Atualizando usuário com foto de perfil default

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,7 +2,10 @@ import Header from "../../components/Header";
 import React from "react";
 
 function Profile(props) {
+
+    const user = JSON.parse(localStorage.getItem("user"));
     return (
+        console.log(user),
         <Header />
     );
 };
