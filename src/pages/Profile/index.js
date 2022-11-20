7444508b2a66b892078594bd4f140503f310df94
Samuel Lucas <samuellucasvmcontato@gmail.com>
2022-11-20 12:31:21 -0300

    feat: making a profile page with db info

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,10 +2,38 @@ import Header from "../../components/Header";
 import React from "react";
 import api from "../../service/index";
 import { useState, useEffect } from "react";
+import "./styles.css";
 
+function Profile() {
+    const [profileInfo, setProfileInfo] = useState([]);
+    const [name, setName] = useState();
+    const [age, setAge] = useState();
 
-function Profile(props) {
-    let [profileInfo, setProfileInfo] = useState([]);
+    async function changeName(e) {
+        let name = e.target.value;
+
+        const response = await api.put("/users/info", { name: name }, {
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": "Bearer " + localStorage.getItem("token")
+            }
+        });
+        name = response.data.name;
+        setName(name);
+    };
+
+    async function changeAge(e) {
+        let age = e.target.value;
+
+        const response = await api.put("/users/info", { age: age }, {
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": "Bearer " + localStorage.getItem("token")
+            }
+        });
+        age = response.data.age;
+        setName(name);
+    };
 
     async function getUserInfo() {
         const response = await api.get(
@@ -22,8 +50,17 @@ function Profile(props) {
     return (
         <>
             <Header />
-            <button onClick={() => console.log(profileInfo.data)}></button>
-            <img href=''></img>
+            <div id='profile-infos'>
+                <button>
+                    <img id='profile-photo' src={profileInfo.data && profileInfo.data.photo}></img>
+                </button>
+                <div id='profile-inputs'>
+                    <label>Nome do usuário:</label>
+                    <input type='text' className='profile-input' defaultValue={profileInfo.data && profileInfo.data.name} onChange={changeName}></input>
+                    <label>Idade do usuário:</label>
+                    <input type='number' className='profile-input' defaultValue={profileInfo.data && profileInfo.data.age} onChange={changeAge}></input>
+                </div>
+            </div>
         </>
     );
 };
